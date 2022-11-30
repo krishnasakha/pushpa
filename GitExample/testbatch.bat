ddc6e0bfbdc2f5a8af32303f6126770876286943
krishnasakha <pushpalathag061@gmail.com>
2022-11-30 18:23:28 +0530

    Im adding testngbatchfile and lib

--- GitExample/testbatch.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\sakha\eclipse-workspace\testing
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*;
-java org.testng.TestNG TestFrames.xml
-pause
